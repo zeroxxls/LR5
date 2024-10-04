97c24932ceef3c77a92f05a2c6b32563e0539d69
zeroxxl <zeroxxhls@gmail.com>
2024-10-04 13:50:38 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
+// Get unique categories
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className='menu section'>
+        <div className='title'>
+          <h2>Our menu</h2>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
+}
+
+export default App;
+
